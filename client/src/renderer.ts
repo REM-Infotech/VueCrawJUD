b0e15f7e5e7c04f25a447a41687be5a2bc129c83
Robotz213 <nicholas@robotz.dev>
2025-03-13 15:48:28 -0400

    [feat] remove renderer.ts file
    - Deleted the renderer.ts file which contained the main Vue application setup.
    - Removed imports for CSS and JS dependencies related to Bootstrap and main application styles.

--- client/src/renderer.ts
+++
@@ -1,8 +0,0 @@
-import "./assets/main.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import { createApp } from "vue";
-import App from "./App.vue";
-
-const app = createApp(App);
-app.mount("#app");
