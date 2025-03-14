d0b1e9c840bc48410be0e040aaa9eacede0db1ff
Nicholas Silva <nicholasazs213@gmail.com>
2025-03-13 22:22:09 -0400

    ``` [feat] update modal integration and add button functionality
    - DashboardView.vue: integrated new modal functionality
    - LoginView.vue: refactored to ensure correct modal initialization
    - Added button functionality for improved user interaction
    ```

--- client/src/routes/dashboard/components/ChartComponent.vue
+++ client/src/routes/dashboard/components/ChartComponent.vue
@@ -6,7 +6,11 @@
           <i class="fas fa-chart-bar me-1"></i>
           Execuções por Mês
         </div>
-        <div class="card-body"><canvas id="month_chart" width="100%" height="40"></canvas></div>
+        <div class="card-body d-flex justify-content-center">
+          <div class="d-flex justify-content-center p-4" style="height: 35vh">
+            <canvas id="month_chart"></canvas>
+          </div>
+        </div>
       </div>
     </div>
     <div class="col-xl-6">
@@ -15,16 +19,21 @@
           <i class="fas fa-chart-pie me-1"></i>
           Robôs mais utilizados
         </div>
-        <div class="card-body"><canvas id="most_executed" width="100%" height="40"></canvas></div>
+        <div class="card-body d-flex justify-content-center">
+          <div class="d-flex justify-content-center p-4" style="height: 35vh">
+            <canvas id="most_executed"></canvas>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Chart } from "chart.js";
+import { Chart } from "chart.js/auto";
 import jQuery from "jquery";
-
+import { onMounted } from "vue";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const $ = jQuery;
 
 // Set new default font family and font color to mimic Bootstrap's default styling
@@ -32,133 +41,254 @@ Chart.defaults.font.family =
   '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.color = "#292b2c";
 
-var month_chart = new Chart("month_chart", {
-  type: "bar",
-  data: {
-    labels: [
-      "Janeiro",
-      "Fevereiro",
-      "Março",
-      "Abril",
-      "Maio",
-      "Junho",
-      "Julho",
-      "Agosto",
-      "Setembro",
-      "Outubro",
-      "Novembro",
-      "Dezembro",
-    ],
+onMounted(() => {
+  const ctx_month = document.getElementById("month_chart") as HTMLCanvasElement;
+  if (!ctx_month) return;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  var month_chart = new Chart(ctx_month, {
+    type: "bar",
+    data: {
+      labels: [
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro",
+      ],
 
-    datasets: [
-      {
-        label: "Execuções",
-        backgroundColor: [
-          "#FF9800",
-          "#FF5722",
-          "#795548",
-          "#9E9E9E",
-          "#607D8B",
-          "#F44336",
-          "#E57373",
-          "#BA68C8",
-          "#8D6E63",
-          "#D84315",
-          "#A1887F",
-          "#4E342E",
-          "#3E2723",
-          "#D32F2F",
-          "#FFA726",
-          "#EF6C00",
-          "#BCAAA4",
-          "#A1887F",
-          "#8E24AA",
-          "#5D4037",
-          "#7B1FA2",
-          "#8E8E8E",
-          "#6D4C41",
-          "#FF7043",
-          "#FF8A65",
-          "#6A1B9A",
-          "#FFAB91",
-          "#F57F17",
-          "#B39DDB",
-          "#9C27B0",
-          "#E65100",
-          "#6A1B9A",
-          "#D4E157",
-          "#9C9C9C",
-          "#BF360C",
-          "#FFCCBC",
-          "#616161",
-          "#BDBDBD",
-          "#8D6E63",
-          "#C62828",
-        ],
-        borderColor: [
-          "#CC7A00",
-          "#CC471A",
-          "#5D4037",
-          "#7E7E7E",
-          "#4A5C6B",
-          "#CC3726",
-          "#CC4F4F",
-          "#9546A8",
-          "#715A54",
-          "#AB3413",
-          "#826F66",
-          "#3D2E2A",
-          "#31211F",
-          "#B72B29",
-          "#E48920",
-          "#CB5C00",
-          "#9A7D73",
-          "#826F66",
-          "#7A1D90",
-          "#4C3B34",
-          "#681C85",
-          "#737373",
-          "#583F37",
-          "#CC5C37",
-          "#CC7557",
-          "#581682",
-          "#CC9171",
-          "#C26D14",
-          "#977BB8",
-          "#811B92",
-          "#B34700",
-          "#581682",
-          "#A4BD41",
-          "#808080",
-          "#99310B",
-          "#CCAC9C",
-          "#494949",
-          "#989898",
-          "#715A54",
-          "#A32222",
-        ],
-        data: [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
-      },
-    ],
-  },
-});
+      datasets: [
+        {
+          label: "Execuções",
+          backgroundColor: [
+            "#FF9800",
+            "#FF5722",
+            "#795548",
+            "#9E9E9E",
+            "#607D8B",
+            "#F44336",
+            "#E57373",
+            "#BA68C8",
+            "#8D6E63",
+            "#D84315",
+            "#A1887F",
+            "#4E342E",
+            "#3E2723",
+            "#D32F2F",
+            "#FFA726",
+            "#EF6C00",
+            "#BCAAA4",
+            "#A1887F",
+            "#8E24AA",
+            "#5D4037",
+            "#7B1FA2",
+            "#8E8E8E",
+            "#6D4C41",
+            "#FF7043",
+            "#FF8A65",
+            "#6A1B9A",
+            "#FFAB91",
+            "#F57F17",
+            "#B39DDB",
+            "#9C27B0",
+            "#E65100",
+            "#6A1B9A",
+            "#D4E157",
+            "#9C9C9C",
+            "#BF360C",
+            "#FFCCBC",
+            "#616161",
+            "#BDBDBD",
+            "#8D6E63",
+            "#C62828",
+          ],
+          borderColor: [
+            "#CC7A00",
+            "#CC471A",
+            "#5D4037",
+            "#7E7E7E",
+            "#4A5C6B",
+            "#CC3726",
+            "#CC4F4F",
+            "#9546A8",
+            "#715A54",
+            "#AB3413",
+            "#826F66",
+            "#3D2E2A",
+            "#31211F",
+            "#B72B29",
+            "#E48920",
+            "#CB5C00",
+            "#9A7D73",
+            "#826F66",
+            "#7A1D90",
+            "#4C3B34",
+            "#681C85",
+            "#737373",
+            "#583F37",
+            "#CC5C37",
+            "#CC7557",
+            "#581682",
+            "#CC9171",
+            "#C26D14",
+            "#977BB8",
+            "#811B92",
+            "#B34700",
+            "#581682",
+            "#A4BD41",
+            "#808080",
+            "#99310B",
+            "#CCAC9C",
+            "#494949",
+            "#989898",
+            "#715A54",
+            "#A32222",
+          ],
+          data: [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+    },
+  });
 
-$.ajax({
-  url: "/PerMonth",
-  type: "GET",
-  success: function (data) {
-    month_chart.data?.labels?.forEach((item, pos) => {
-      for (const label of data.labels) {
-        if (
-          month_chart.data.labels &&
-          month_chart.data.labels[pos].toLowerCase() === label.toLowerCase()
-        ) {
-          month_chart.data.datasets[0].data[pos] = data.values[pos];
-          break;
-        }
-      }
-    });
-    month_chart.update();
-  },
+  const ctx_most_executed = document.getElementById("most_executed") as HTMLCanvasElement;
+  if (!ctx_most_executed) return;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  var most_executed = new Chart(ctx_most_executed, {
+    type: "doughnut",
+
+    data: {
+      datasets: [
+        {
+          label: "Execuções",
+          backgroundColor: [
+            "#FF9800",
+            "#FF5722",
+            "#795548",
+            "#9E9E9E",
+            "#607D8B",
+            "#F44336",
+            "#E57373",
+            "#BA68C8",
+            "#8D6E63",
+            "#D84315",
+            "#A1887F",
+            "#4E342E",
+            "#3E2723",
+            "#D32F2F",
+            "#FFA726",
+            "#EF6C00",
+            "#BCAAA4",
+            "#A1887F",
+            "#8E24AA",
+            "#5D4037",
+            "#7B1FA2",
+            "#8E8E8E",
+            "#6D4C41",
+            "#FF7043",
+            "#FF8A65",
+            "#6A1B9A",
+            "#FFAB91",
+            "#F57F17",
+            "#B39DDB",
+            "#9C27B0",
+            "#E65100",
+            "#6A1B9A",
+            "#D4E157",
+            "#9C9C9C",
+            "#BF360C",
+            "#FFCCBC",
+            "#616161",
+            "#BDBDBD",
+            "#8D6E63",
+            "#C62828",
+          ],
+          borderColor: [
+            "#CC7A00",
+            "#CC471A",
+            "#5D4037",
+            "#7E7E7E",
+            "#4A5C6B",
+            "#CC3726",
+            "#CC4F4F",
+            "#9546A8",
+            "#715A54",
+            "#AB3413",
+            "#826F66",
+            "#3D2E2A",
+            "#31211F",
+            "#B72B29",
+            "#E48920",
+            "#CB5C00",
+            "#9A7D73",
+            "#826F66",
+            "#7A1D90",
+            "#4C3B34",
+            "#681C85",
+            "#737373",
+            "#583F37",
+            "#CC5C37",
+            "#CC7557",
+            "#581682",
+            "#CC9171",
+            "#C26D14",
+            "#977BB8",
+            "#811B92",
+            "#B34700",
+            "#581682",
+            "#A4BD41",
+            "#808080",
+            "#99310B",
+            "#CCAC9C",
+            "#494949",
+            "#989898",
+            "#715A54",
+            "#A32222",
+          ],
+          data: [0.1, 0.1, 0.1],
+        },
+      ],
+      labels: ["Carregando", "Carregando", "Carregando"],
+    },
+    options: {
+      responsive: true,
+    },
+  });
+
+  // $.ajax({
+  //   url: "/most_executed",
+  //   type: "GET",
+  //   success: function (data) {
+  //     most_executed.data.labels = data.labels;
+  //     most_executed.data.datasets[0].data = data.values;
+  //     most_executed.update();
+  //   },
+  // });
+  // $.ajax({
+  //   url: "/PerMonth",
+  //   type: "GET",
+  //   success: function (data) {
+  //     month_chart.data?.labels?.forEach((item, pos) => {
+  //       for (const label of data.labels) {
+  //         if (
+  //           month_chart.data.labels &&
+  //           month_chart.data.labels[pos].toLowerCase() === label.toLowerCase()
+  //         ) {
+  //           month_chart.data.datasets[0].data[pos] = data.values[pos];
+  //           break;
+  //         }
+  //       }
+  //     });
+  //     month_chart.update();
+  //   },
+  // });
 });
 </script>
