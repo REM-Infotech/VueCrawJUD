5b8541ed7f1802b0accb9e45a6731358a78c3003
Nicholas Silva <nicholasazs213@gmail.com>
2025-03-14 21:42:11 -0300

    [feat] implement new logging mechanism for better error tracking
    - chore
      - logger.js: added logging functionality to capture errors

--- client/src/routes/dashboard/components/requests.ts
+++ client/src/routes/dashboard/components/requests.ts
@@ -15,7 +15,7 @@ export async function getExecutions() {
       let link_file = item.file_output;
 
       if (status.toLowerCase() !== "em execução") {
-        link_file = `<a href="${link_file}" target="_blank">${item.file_output}</a>`;
+        link_file = `<a class="btn btn-sm btn-success" href="${link_file}" target="_blank">Baixar Arquivo</a>`;
       }
 
       return [
