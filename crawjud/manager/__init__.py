fd938c1e13cb1391ce3c0d1fb7ab68bf24bfa961
Robotz213 <nicholas@robotz.dev>
2025-04-01 21:12:30 +0000

    refactor: remove obsolete files and clean up project structure

--- crawjud/manager/__init__.py
+++
@@ -1,8 +0,0 @@
-"""CrawJUD Manager Package."""
-
-from .menu import MenuManager
-from .runner import RunnerServices
-
-
-class HeadCrawjudManager(MenuManager, RunnerServices):
-    """Head class for CrawJUDManager."""
