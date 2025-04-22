959d5737e07af4362fea21c517da754504c2ba65
Robotz213 <nicholas@robotz.dev>
2025-04-22 15:29:18 -0300

    refactor: Enhance context menu and import structure in components.ts
    src/main/components.ts:
    - Added 'app' and 'Menu' imports from 'electron'.
    - Implemented a context menu for the tray icon with a quit option.

--- src/main/components.ts
+++ src/main/components.ts
@@ -1,11 +1,21 @@
-import { Notification, Tray } from "electron";
+import { app, Menu, Notification, Tray } from "electron";
 import { join } from "path";
 const icon = join(process.cwd(), "src", "renderer", "assets", "img", "icon.png");
 
-
 export const NotificationSuccess = new Notification({
   title: "Sucesso!",
-
+  icon: icon
 });
 
 export const IconTray = new Tray(icon);
+
+
+const MenuApp = Menu.buildFromTemplate([
+  {
+    label: "Sair",
+    click() {
+      app.quit();
+    }
+  }
+])
+IconTray.setContextMenu(MenuApp)
