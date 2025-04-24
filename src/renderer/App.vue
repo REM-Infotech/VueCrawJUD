e8335ba86bc544b4801555b49e1caa04f93d5545
Robotz213 <nicholas@robotz.dev>
2025-04-24 18:32:31 -0300

    feat: Add LogBotView and ChartProgress components
    - src/renderer/pages/logs/LogBotView.vue: new component for displaying log statistics and real-time updates
    - src/renderer/pages/logs/components/ChartProgress.vue: new component for visualizing log progress with a chart
    - src/renderer/pages/logs/components/LogsView.vue: new component for displaying log messages
    refactor: Remove unused footer from App.vue
    - src/renderer/App.vue: cleaned up footer section

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -15,40 +15,6 @@ import SideBarComponent from "./components/SideBarComponent.vue";
     </div>
   </main>
   <ModalLoading />
-  <footer class="mt-auto bg-dark text-white p-3" data-bs-theme="dark">
-    <span>© 2025 Company, Inc</span>
-  </footer>
-  <!-- <footer
-
-    data-bs-theme="dark"
-  >
-    <div class="col-md-4 d-flex align-items-center">
-      <a
-        href="/"
-        class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1"
-        aria-label="Bootstrap"
-      >
-        <svg class="bi" width="30" height="24" aria-hidden="true">
-          <use xlink:href="#bootstrap"></use>
-        </svg>
-      </a>
-
-    </div>
-
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-      <li class="ms-3">
-        <a class="text-body-secondary" href="#" aria-label="Instagram"
-          ><svg class="bi" width="24" height="24" aria-hidden="true">
-            <use xlink:href="#instagram"></use></svg
-        ></a>
-      </li>
-      <li class="ms-3">
-        <a class="text-body-secondary" href="#" aria-label="Facebook"
-          ><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg
-        ></a>
-      </li>
-    </ul>
-  </footer> -->
 </template>
 
 <style>
--- src/shared/ElectronConfig.ts
+++ src/shared/ElectronConfig.ts
@@ -1,6 +1,5 @@
 import type { LoadWindow, TtitleBarStyle } from "@/@types/service";
 import type { BrowserWindow } from "electron";
-import isDev from "electron-is-dev";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
@@ -18,8 +17,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const titleBarStyle: TtitleBarStyle = () => {
-  if (isDev) {
-    return "default";
-  }
+  // if (isDev) {
+  //   return "default";
+  // }
   return "hidden";
 };
