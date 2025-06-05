7e2ec6d2e8fbc8c17f937e98cd973a1e59077932
Robotz213 <nicholas@robotz.dev>
2025-06-05 17:36:41 -0400

    ✨ Update CardsView component with filtering functionality
    src/views/robots/components/CardsView.vue: Implemented a filtering feature for the list of categories, updated the card header layout, and added transition effects for list items.

--- src/views/robots/components/CardsView.vue
+++ src/views/robots/components/CardsView.vue
@@ -1,17 +1,44 @@
 <script setup lang="ts">
 import { faAngleRight, faWarning } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { computed, ref } from "vue";
 
 const listItems = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+const list = [
+  { msg: "Bruce Lee" },
+  { msg: "Jackie Chan" },
+  { msg: "Chuck Norris" },
+  { msg: "Jet Li" },
+  { msg: "Kung Fury" },
+];
+
+const query = ref("");
+
+const computedList = computed(() => {
+  return list.filter((item) => item.msg.toLowerCase().includes(query.value));
+});
 </script>
 
 <template>
   <div class="row">
     <div class="col-md-3">
       <div class="card" style="height: 75dvh">
-        <div class="card-header">Listagem de robôs</div>
+        <div class="card-header d-flex">
+          <span class="title align-items-center d-flex">
+            <IBiPieChartFill />
+            <span class="ms-1"> Categorias </span>
+          </span>
+          <div class="ms-auto">
+            <input class="form-control" placeholder="Filtre aqui.." type="text" v-model="query" />
+          </div>
+        </div>
         <div class="card-body">
-          <div class="card-title fs-5">Categorias de robôs</div>
+          <TransitionGroup name="slide-fade" class="list-group" tag="ul">
+            <li class="list-group-item" v-for="(item, index) in computedList" :key="index">
+              {{ item.msg }}
+            </li>
+          </TransitionGroup>
         </div>
       </div>
     </div>
@@ -38,3 +65,23 @@ const listItems = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
     </div>
   </div>
 </template>
+
+<style lang="css" scoped>
+/*
+  Enter and leave animations can use different
+  durations and timing functions.
+*/
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
