2883ac8b4416a4d9b62bdad85832562d21449acd
Robotz213 <nicholas@robotz.dev>
2025-06-10 21:15:12 -0300

    ✨ Add socket.io integration and update EmptyView component
    .env: Create environment file with VITE_API_URL configuration.
    env.d.ts: Define ImportMetaEnv interface to include VITE_API_URL.
    package.json: Add socket.io-client as a dependency.
    package-lock.json: Update lock file to include socket.io-client.
    src/resources/socketio.ts: Implement socket.io client with server URI from environment variable.
    src/views/EmptyView.vue: Enhance component with a button to emit messages via socket.io.

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,19 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  url: string;
+
+  readonly hot?: import("./hot").ViteHotContext;
+
+  readonly env: ImportMetaEnv;
+
+  glob: import("./importGlob").ImportGlobFunction;
+}
+
 declare module "*.js";
 type CSSModuleClasses = { readonly [key: string]: string };
 
--- src/views/EmptyView.vue
+++ src/views/EmptyView.vue
@@ -1,1 +1,18 @@
-<template>teste</template>
+<template>
+  <div class="container bg-white text-black">
+    teste
+    <button class="btn btn-success" @click="emitmessage">teste!</button>
+  </div>
+</template>
+<script setup lang="ts">
+import io from "@/resources/socketio";
+
+function emitmessage() {
+  io.emit("teste", { data: "teste", file: "teste" });
+}
+
+io.auth = {
+  token: "teste",
+};
+io.connect();
+</script>
