4acf43982c7f1b1dd0189f984c2f88d961fad8b9
Robotz213 <nicholas@robotz.dev>
2025-06-12 12:48:06 -0300

    🔧 Refactor error handling in authentication handler
    src/views/auth/handlerAuthentication.ts: Improve error handling by checking for Axios errors before logging. Added a conditional return for development mode.

--- src/views/auth/handlerAuthentication.ts
+++ src/views/auth/handlerAuthentication.ts
@@ -1,13 +1,19 @@
 import { api } from "@/main";
 import type { LoginForm } from "@/types/forms";
+import { isAxiosError } from "axios";
 
 export async function handleAuthentication(form: LoginForm) {
   try {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const result = await api.request("post", "/login", form);
   } catch (err) {
-    console.log(err);
+    if (isAxiosError(err)) {
+      console.log(err);
+    }
   }
 
+  if (import.meta.env.VITE_DEVMODE) {
+    return true;
+  }
   return true;
 }
