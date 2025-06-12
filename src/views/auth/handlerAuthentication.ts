8ebb407bd666b8bd6c18ccb924442572e64de60b
Robotz213 <nicholas@robotz.dev>
2025-06-12 17:37:42 -0300

    🔧 Update cookie settings for authentication
    src/views/auth/handlerAuthentication.ts: Enable HttpOnly flag for access, refresh, and admin cookies to enhance security.

--- src/views/auth/handlerAuthentication.ts
+++ src/views/auth/handlerAuthentication.ts
@@ -14,21 +14,21 @@ export async function handleAuthentication(form: LoginForm) {
         expires: Date.parse(data?.access?.expiration as string),
         secure: true,
         sameSite: "Strict",
-        // HttpOnly: true,
+        HttpOnly: true,
       });
 
       appCookies.insertKey("refresh_token", data?.refresh?.token as string, {
         expires: Date.parse(data?.refresh?.expiration as string),
         secure: true,
         sameSite: "Strict",
-        // HttpOnly: true,
+        HttpOnly: true,
       });
 
       appCookies.insertKey("isAdmin", data?.isAdmin?.toString() as string, {
         expires: Date.parse(data?.refresh?.expiration as string),
         secure: true,
         sameSite: "Strict",
-        // HttpOnly: true,
+        HttpOnly: true,
       });
     }
   } catch (err) {
